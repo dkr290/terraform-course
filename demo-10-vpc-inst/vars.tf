4edcd77873db314b0442eb3109b782a75f1b67cb
dani <d_krasimirov@yahoo.com>
2021-12-11 21:44:15 +0100

    changesto cloudinit vpc etc

--- demo-10-vpc-inst/vars.tf
+++ demo-10-vpc-inst/vars.tf
@@ -1,7 +1,6 @@
 variable "AWS_REGION" {
-  default = "eu-west-1"
+  default = "eu-central-1"
 }
-
 variable "PATH_TO_PRIVATE_KEY" {
   default = "mykey"
 }
@@ -16,10 +15,10 @@ variable "AMIS" {
     us-east-1 = "ami-13be557e"
     us-west-2 = "ami-06b94666"
     eu-west-1 = "ami-844e0bf7"
+    eu-central-1= "ami-0a49b025fffbbdac6"
   }
 }
 
 variable "INSTANCE_DEVICE_NAME" {
   default = "/dev/xvdh"
 }
-
